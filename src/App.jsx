9e1f13fa41acfce54166f5efcb74987570ff340d
Pooja-chaudhary960 <pujadahit52@gmail.com>
2026-08-05 23:53:50 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div><h1 class="text-3xl font-bold text-blue-600">Hello World!</h1></div>
+    </>
+  )
+}
+
+export default App
